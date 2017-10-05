79ca718b2f08341a733c7f9436874e4de71725d5
drewxa <a.borodin@bmstu.ru>
2017-10-05 15:56:43 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,5 +3,12 @@
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    char a = 0;
+    char b = 0;
+    char c = 0;
+    int d = 0;
+    int e = 0;
+    std::cin >> a >> b >> d >> c >> e;
+    std::cout << a << b << d << c << e << std::endl; 
     return 0;
 }
